4fc1d1448205e2bb91c850da42e81e0a8554822d
Brian <blsvsanteliz@gmail.com>
2021-01-16 18:12:39 -0400

    add typescript

---
+++ components/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header } from "./";
+const App = () => {
+  return (
+    <>
+      <Header />
+      Welcome to React & Snowpack!
+    </>
+  );
+};
+
+export default App;
--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./index.css";
 
 const rootElement = document.getElementById("root");
-
-ReactDOM.render(<h1>Hola desde React & SNOWPACJ</h1>, rootElement);
+ReactDOM.render(<App />, rootElement);
